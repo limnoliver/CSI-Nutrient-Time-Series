e8b8ddcd1f2a8dcdb312ce70d7a417524597cb24
limnoliver <skoliver@wisc.edu>
2016-06-16 14:44:30 -0500

    Added code to also generate a unique stoich dataframe with TN:TP (using same filtering criteria as TN, TP and Chl)

--- Code/LIMNO_data_processing.R
+++ Code/LIMNO_data_processing.R
@@ -26,16 +26,20 @@ data$tn_combined[which(is.na(data$tn_combined) == TRUE)] = data$tn_calculated[wh
 tn = data[is.na(data$tn_combined)==FALSE, ]
 tp = data[is.na(data$tp)==FALSE, ]
 chl = data[is.na(data$chla)==FALSE, ]
+stoich = data[is.na(data$tn_combined)==FALSE & is.na(data$tp)==FALSE, ]
 
 #create molar version of nutrients
 tn$tn_umol = tn$tn_combined/14.01
 tp$tp_umol = tp$tp/30.97
+stoich$tn_tp = stoich$tn_combined/stoich$tp
+stoich$tn_tp_umol = stoich$tn_tp * (30.97/14.01)
 
 #limit observations from JUN 15-AUG 15
 # first, just keep samples in JUN, JUL, AUG, SEP
 tn.summer = tn[tn$samplemonth == 6|tn$samplemonth == 7|tn$samplemonth==8|tn$samplemonth==9, ]
 tp.summer = tp[tp$samplemonth == 6|tp$samplemonth == 7|tp$samplemonth==8|tp$samplemonth==9, ]
 chl.summer = chl[chl$samplemonth == 6|chl$samplemonth == 7|chl$samplemonth==8|chl$samplemonth==9, ]
+stoich.summer = stoich[stoich$samplemonth == 6|stoich$samplemonth == 7|stoich$samplemonth==8|stoich$samplemonth==9, ]
 
 # now, get rid of first half of June and second half of Sept
 tn.summer$sampleday = format(tn.summer$sampledate, "%d")
@@ -46,6 +50,9 @@ tp.summer$sampleday = as.numeric(tp.summer$sampleday)
 
 chl.summer$sampleday = format(chl.summer$sampledate, "%d")
 chl.summer$sampleday = as.numeric(chl.summer$sampleday)
+
+stoich.summer$sampleday = format(stoich.summer$sampledate, "%d")
+stoich.summer$sampleday = as.numeric(stoich.summer$sampleday)
 
 keep.summer <- function(x) {
 keep.june = which(x$samplemonth == 6 & x$sampleday >= 15)
@@ -61,6 +68,12 @@ return(x[keep.all, ])
 tn.summer = keep.summer(tn.summer)
 tp.summer = keep.summer(tp.summer)
 chl.summer = keep.summer(chl.summer)
+stoich.summer = keep.summer(stoich.summer)
+
+#remove all points where TN:TP > 1000. This is based on data from Downing & McCauley, where
+#the max epilimnetic value from the world's lakes was ~ 552. A rough doubling of that gives TN:TP (molar)
+#of 1,000, to be conservative. 
+stoich.summer = stoich.summer[stoich.summer$tn_tp_umol < 1000, ]
 
 
 #find mean, coefficient of variance (covar) and number of observations
@@ -70,30 +83,7 @@ intra.annual.tp = aggregate(tp.summer$tp_umol, tp.summer[,c("lagoslakeid", "samp
 intra.annual.tn.secchi = aggregate(tn.summer$secchi, tn.summer[,c("lagoslakeid", "sampleyear")], FUN=function(x) c(mean=mean(x), sd=sd(x), covar=sd(x)/mean(x), nobs=length(x)))
 intra.annual.tp.secchi = aggregate(tp.summer$secchi, tp.summer[,c("lagoslakeid", "sampleyear")], FUN=function(x) c(mean=mean(x), sd=sd(x), covar=sd(x)/mean(x), nobs=length(x)))
 intra.annual.chl = aggregate(chl.summer$chla, chl.summer[,c("lagoslakeid", "sampleyear")], FUN=function(x) c(mean=mean(x),sd=sd(x), covar=sd(x)/mean(x), nobs=length(x)))
-
-year.means.tn = data.frame(lagoslakeid = intra.annual.tn$lagoslakeid, 
-                             sampleyear = intra.annual.tn$sampleyear, 
-                             tn_umol = intra.annual.tn$x[,1], 
-                             sd = intra.annual.tn$x[,2], 
-                             covar = intra.annual.tn$x[,3], 
-                             nobs = intra.annual.tn$x[,4])
-
-
-
-year.means.tp = data.frame(lagoslakeid = intra.annual.tp$lagoslakeid, 
-                           sampleyear = intra.annual.tp$sampleyear, 
-                           tp_umol = intra.annual.tp$x[,1], 
-                           sd = intra.annual.tp$x[,2], 
-                           covar = intra.annual.tp$x[,3], 
-                           nobs = intra.annual.tp$x[,4])
-
-year.means.chl = data.frame(lagoslakeid = intra.annual.chl$lagoslakeid,
-                            sampleyear = intra.annual.chl$sampleyear, 
-                            chl = intra.annual.chl$x[,1], 
-                            sd = intra.annual.chl$x[,2], 
-                            covar = intra.annual.chl$x[,3], 
-                            nobs = intra.annual.chl$x[,4])
-
+intra.annual.tntp = aggregate(stoich.summer$tn_tp_umol, stoich.summer[,c("lagoslakeid", "sampleyear")], FUN=function(x) c(mean=mean(x),sd=sd(x), covar=sd(x)/mean(x), nobs=length(x)))
 
 year.means.tn.secchi = data.frame(lagoslakeid = intra.annual.tn.secchi$lagoslakeid, 
                            sampleyear = intra.annual.tn.secchi$sampleyear, 
@@ -121,6 +111,15 @@ year.means.tp = data.frame(lagoslakeid = year.means.tp$lagoslakeid,
                            secchi = year.means.tp.secchi$secchi,
                            nobs = year.means.tp$nobs)
 
+year.means.tntp = data.frame(lagoslakeid = intra.annual.tntp$lagoslakeid, 
+                             sampleyear = intra.annual.tntp$sampleyear, 
+                             tn_tp_umol = intra.annual.tntp$x[,1], 
+                             nobs = intra.annual.tntp$x[,4])
+
+year.means.chl = data.frame(lagoslakeid = intra.annual.chl$lagoslakeid, 
+                             sampleyear = intra.annual.chl$sampleyear, 
+                             chl = intra.annual.chl$x[,1], 
+                             nobs = intra.annual.chl$x[,4])
 
 # merge with Iowa data that was received from J. Downing and has been 
 # processed in teh file "Iowa lakes import.R"
@@ -130,10 +129,12 @@ iowa = read.csv("Iowa_LAGOS merged.csv", header = TRUE)
 
 year.means.tn = rbind(year.means.tn, iowa[,c(2,3,4,7,8)])
 year.means.tp = rbind(year.means.tp, iowa[,c(2,3,5,7,8)])
+year.means.tntp = rbind(year.means.tntp, iowa[,c(2,3,6,8)])
 
 #limit analysis to only 1990-present
 modern.tn = year.means.tn[year.means.tn$sampleyear > 1989, ]
 modern.tp = year.means.tp[year.means.tp$sampleyear > 1989, ]
+modern.tntp = year.means.tntp[year.means.tntp$sampleyear > 1989, ]
 modern.chl = year.means.chl[year.means.chl$sampleyear > 1989, ]
 
 #limit to lakes that have at least one observation in each decade of time extent
@@ -161,6 +162,7 @@ return(x[x$lagoslakeid %in% lakes.10, ])
 
 modern.tn.e10 = occurance.filter(modern.tn)
 modern.tp.e10 = occurance.filter(modern.tp)
+modern.tntp.e10 = occurance.filter(modern.tntp)
 modern.chl.e10 = occurance.filter(modern.chl)
 
 #limit to lakes that cover at least 5 years in duration
@@ -183,7 +185,8 @@ return(x[x$lagoslakeid %in% lakes, ])
 }
 modern.chl.e10 = duration.filter(modern.chl.e10)
 modern.tp.e10 = duration.filter(modern.tp.e10)
-modern.tn.e10 = duration.filtern(modern.tn.e10)
+modern.tn.e10 = duration.filter(modern.tn.e10)
+modern.tntp.e10 = duration.filter(modern.tntp.e10)
 
 # create a mixed model where the nutrient is the response, year is the 
 # the predictor, and slopes and intercepts are allowed to vary by lake (grouping)
@@ -191,9 +194,11 @@ modern.tn.e10 = duration.filtern(modern.tn.e10)
 modern.tp.e10$sampleyear_cor = modern.tp.e10$sampleyear - 1990
 modern.tn.e10$sampleyear_cor = modern.tn.e10$sampleyear - 1990
 modern.chl.e10$sampleyear_cor = modern.chl.e10$sampleyear - 1990
+modern.tntp.e10$sampleyear_cor = modern.tntp.e10$sampleyear - 1990
 
 #write data files
 setwd("C:/Users/Samantha/Dropbox/CSI_LIMNO_Manuscripts-presentations/CSI_Nitrogen MSs/Time series/Data")
 write.table(modern.tn.e10, "timeseries_data_tn.txt")
 write.table(modern.tp.e10, "timeseries_data_tp.txt")
 write.table(modern.chl.e10, "timeseries_data_chl.txt")
+write.table(modern.tntp.e10, "timeseries_data_tntp.txt")
