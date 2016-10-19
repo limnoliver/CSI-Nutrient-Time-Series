dd44277bdcbc8fb8705fae6bbb9526f7150d5b7b
limnoliver <skoliver@wisc.edu>
2016-10-19 16:45:50 -0500

    take some code from rf script, separate by analysis and predictor processing

---
+++ Code/RF_predictor_processing.R
@@ -0,0 +1,17 @@
+## This file prepares the predictor variables data frame for the random forest analysis
+## first merges all relevant variables from various LAGOS geo tables
+
+## for lake-specific slopes, include:
+## 1) LULC at buffer scale (% ag, % forest, % urban, %wetland)
+## 2) lake-specific features (depth, area)
+## 3) connectivity type
+## 4) topography
+## 5) model parameters: number of years of observation, median year, mean concentration
+
+## for region-specific slopes, include:
+## 1) LULC at huc 4 scale (% ag, %forest, % urban)
+## 2) climate at the huc4 scale (summer, yearly temp and precip)
+## 3) connectivity/hydrology at huc 4 scale (water in region, runoff, etc)
+## 4) atmospheric deposition at huc 4 scale
+## 5) topography at the huc 4 scale
+## 6) model parameters: number of lakes, mean concentration in region
