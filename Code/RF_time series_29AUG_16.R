844d5a97549a1bc616eb5295e5a36f87f41011d1
Samantha Oliver <skoliver@wisc.edu>
2016-11-01 19:19:19 -1000

    commit all

--- Code/RF_time series_29AUG_16.R
+++ Code/RF_time series_29AUG_16.R
@@ -2,24 +2,6 @@ require(randomForest)
 require(plyr)
 ## use random forest to test which lakes are changing in tn, tp, tn:tp, and chl
 
-## get predictors together
-## for lake-specific slopes, include:
-## 1) LULC at buffer scale (% ag, % forest, % urban, %wetland)
-## 2) lake-specific features (depth, area)
-## 3) connectivity type
-## 4) topography
-## 5) model parameters: number of years of observation, median year, mean concentration
-
-## for region-specific slopes, include:
-## 1) LULC at huc 4 scale (% ag, %forest, % urban)
-## 2) climate at the huc4 scale (summer, yearly temp and precip)
-## 3) connectivity/hydrology at huc 4 scale (water in region, runoff, etc)
-## 4) atmospheric deposition at huc 4 scale
-## 5) topography at the huc 4 scale
-## 6) model parameters: number of lakes, mean concentration in region
-
-## for chlorophyll model, also include TN, TP, TN:TP slopes
-
 ##################################################################
 ## create list of lakes and regions from all nutrient analyses
 ##################################################################
