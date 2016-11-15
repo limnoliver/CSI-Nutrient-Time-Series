b4b0078dd16b494d8031c8281f938ca75d52ef4a
limnoliver <skoliver@wisc.edu>
2016-11-15 15:41:56 -0600

    added merge and import huc4 scale data

--- Code/RF_predictor_processing.R
+++ Code/RF_predictor_processing.R
@@ -21,6 +21,10 @@ setwd("C:/Users/Samantha/Dropbox/CSI_LIMNO_Manuscripts-presentations/CSI_Nitroge
 lakes = read.csv("LAGOS_summer_meanvals.csv", header = TRUE)
 lakes = unique(lakes$lagoslakeid)
 
+##############################################
+## import lake-scale data
+##############################################
+
 ## import lake-specific data (e.g., depth, area, etc)
 setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSData/Version1.054.1")
 all.lakes = read.table("lagos_lakes_10541.txt", 
@@ -37,6 +41,7 @@ all.lakes = all.lakes[,c(1,3:5,12,38)]
 ## limit to lakes in time series analysis
 lakes = subset(all.lakes, all.lakes$lagoslakeid %in% lakes)
 
+## import watershed-scale land use/land cover data
 setwd("~/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
 setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
 
@@ -60,12 +65,31 @@ iws.lulc = iws.lulc[,c(1,8,146,149,155,156,159:162)]
 
 names(iws.lulc)[6] = "lagoslakeid" 
 
-
-
-
 # filter IWS to lakes in time series analysis
 
-geo.data = merge(iws.lulc, iws.lulc$lagoslakeid %in% lakes)
+geo.data = merge(lakes, iws.lulc, by = "lagoslakeid", all.x = TRUE)
 
+# import watershed-scale connectivity metrics
+setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
+iws.conn = read.table("iws_conn.txt", header = TRUE)
+iws.conn = iws.conn[,c(7,12,27,82,84,86,152)]
+names(iws.conn)[7] = "lagoslakeid"
 
+# merge with other geo data
+
+geo.data = merge(geo.data, iws.conn, by = "lagoslakeid", all.x = TRUE)
+
+###################################
+## import huc 4-scale data
+###################################
+
+setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
+# import climate, hydrology, atmospheric, and geology data
+hu4.chag = read.table("hu4_chag.txt", header = TRUE)
+hu4.chag = hu4.chag[,c(1,4,60,76,80,84,92,100)]
+
+# import connectivity metrics
+hu4.conn = read.table("hu4_conn.txt", header = TRUE)
+# import land use/land cover data
+hu4.lulc = read.table("hu4_lulc.txt", header = TRUE)
 
