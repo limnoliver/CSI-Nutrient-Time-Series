28759ea48b876147bb880d1c768d5c840f3ae62a
limnoliver <skoliver@wisc.edu>
2016-11-15 08:20:01 -0600

    added data import

--- Code/RF_predictor_processing.R
+++ Code/RF_predictor_processing.R
@@ -15,3 +15,57 @@
 ## 4) atmospheric deposition at huc 4 scale
 ## 5) topography at the huc 4 scale
 ## 6) model parameters: number of lakes, mean concentration in region
+
+##################################################################
+## create list of lakes and regions from all nutrient analyses
+##################################################################
+
+setwd("C:/Users/Samantha/Dropbox/CSI_LIMNO_Manuscripts-presentations/CSI_Nitrogen MSs/Time series/Data")
+all.lakes = read.table( "lmer_change_db.txt", header = TRUE)
+
+
+# merge with lake-specific info including
+# lat/long, depth, area,
+
+all.lakes = merge(all.lakes,
+                  lake.info[,c("lagoslakeid", "nhd_lat", "nhd_long", "lake_area_ha", "maxdepth",
+                               "hu4_zoneid", "iws_areaha", "lakeconnectivity")],
+                  by = "lagoslakeid",
+                  all.x = TRUE)
+
+################################################
+## create data frame of lake-specific predictors
+################################################
+
+setwd("~/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
+setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
+
+# landuse/land cover data calculated at the watershed scale
+iws.lulc = read.table("iws_lulc.txt", header = TRUE)
+
+# reduce variables down to 2001 LULC data
+# also include topography metrics to test
+
+iws.lulc = iws.lulc[,c(1,49:80,147:156)]
+
+# create urban, row crop, pasture, wetland categories
+
+iws.lulc$iws_urban = iws.lulc$iws_nlcd2001_pct_22 + iws.lulc$iws_nlcd2001_pct_23 + iws.lulc$iws_nlcd2001_pct_24
+iws.lulc$iws_crop = iws.lulc$iws_nlcd2001_pct_82
+iws.lulc$iws_pasture = iws.lulc$iws_nlcd2001_pct_81
+iws.lulc$iws_forest = iws.lulc$iws_nlcd2001_pct_41 + iws.lulc$iws_nlcd2001_pct_42 + iws.lulc$iws_nlcd2001_pct_43
+
+# now narrow predictors
+
+iws.lulc = iws.lulc[,c(1,34:47)]
+
+# get rid of terrain metrics, with the exception of mean slope
+
+iws.lulc = iws.lulc[,c(1,4,10:15)]
+
+
+# rename ID var to allow merge with lake.info
+
+names(iws.lulc)[4] = "lagoslakeid"
+iws.lulc = iws.lulc[,c(2:8)]
+
