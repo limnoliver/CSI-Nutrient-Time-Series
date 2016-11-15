b39e04b4c4d78731ad0fd84fa5db3ef70ed77451
limnoliver <skoliver@wisc.edu>
2016-11-15 16:37:48 -0600

    added atmospheric dep and climate trend analysis

--- Code/RF_predictor_processing.R
+++ Code/RF_predictor_processing.R
@@ -1,3 +1,4 @@
+library(tidyr)
 ## This file prepares the predictor variables data frame for the random forest analysis
 ## first merges all relevant variables from various LAGOS geo tables
 
@@ -25,7 +26,9 @@ lakes = unique(lakes$lagoslakeid)
 ## import lake-scale data
 ##############################################
 
+#############
 ## import lake-specific data (e.g., depth, area, etc)
+
 setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSData/Version1.054.1")
 all.lakes = read.table("lagos_lakes_10541.txt", 
                        header = TRUE, 
@@ -36,11 +39,12 @@ all.lakes = read.table("lagos_lakes_10541.txt",
                        comment.char = "")
 
 ## limit to variables of interest
-all.lakes = all.lakes[,c(1,3:5,12,38)]
+all.lakes = all.lakes[,c(1,14,3:5,12,38)]
 
 ## limit to lakes in time series analysis
 lakes = subset(all.lakes, all.lakes$lagoslakeid %in% lakes)
 
+###########
 ## import watershed-scale land use/land cover data
 setwd("~/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
 setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
@@ -69,6 +73,7 @@ names(iws.lulc)[6] = "lagoslakeid"
 
 geo.data = merge(lakes, iws.lulc, by = "lagoslakeid", all.x = TRUE)
 
+#################
 # import watershed-scale connectivity metrics
 setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
 iws.conn = read.table("iws_conn.txt", header = TRUE)
@@ -84,10 +89,82 @@ geo.data = merge(geo.data, iws.conn, by = "lagoslakeid", all.x = TRUE)
 ###################################
 
 setwd("C:/Users/Samantha/Dropbox/CSI-LIMNO_DATA/LAGOSGeoData/LAGOS_VER1.03")
+
+###############
 # import climate, hydrology, atmospheric, and geology data
 hu4.chag = read.table("hu4_chag.txt", header = TRUE)
-hu4.chag = hu4.chag[,c(1,4,60,76,80,84,92,100)]
 
+# calculate trends in atmospheric deposition
+atm.dep = hu4.chag[,c(1,60,64,68,72,76)]
+atm.dep = gather(atm.dep, key = "year", value = "deposition",2:6)
+temp = gsub("(hu4_dep_totaln_)(\\d+)(_mean)", replacement = "\\2", atm.dep$year)
+atm.dep$year = temp
+
+dep.change = c()
+for (i in 1:length(unique(atm.dep$hu4_zoneid))){
+  dat = atm.dep[atm.dep$hu4_zoneid == unique(atm.dep$hu4_zoneid)[i], ]
+  dat.lm = lm(dat$deposition~as.numeric(dat$year))
+  dep.change[i] = as.numeric(dat.lm$coefficients[2])
+}
+
+dep.change = data.frame(hu4_zoneid = unique(atm.dep$hu4_zoneid),
+                        hu4_dep_change = dep.change)
+
+hu4.chag = hu4.chag[,c(1,4,60,76,84,92,100)]
+hu4.chag = merge(hu4.chag, dep.change, by = "hu4_zoneid", all.x = TRUE)
+
+geo.data = merge(geo.data, hu4.chag, by = "hu4_zoneid", all.x = TRUE)
+
+####################
+# calculate climate trends for each hu4
+setwd("C:/Users/Samantha/Dropbox/CSI_LIMNO_Manuscripts-presentations/CSI_Nitrogen MSs/Time series/Data")
+hu4.ppt.annual = read.csv("HU4_ppt_annual.csv", header=TRUE)
+hu4.tmean.annual = read.csv("HU4_tmean_annual.csv", header = TRUE)
+
+ppt.change = c()
+for (i in 1:length(unique(hu4.ppt.annual$ZoneID))){
+  dat = hu4.ppt.annual[hu4.ppt.annual$ZoneID == unique(hu4.ppt.annual$ZoneID)[i], ]
+  dat.lm = lm((dat$annual/100)~as.numeric(dat$year))
+  ppt.change[i] = as.numeric(dat.lm$coefficients[2])
+}
+ppt.change = data.frame(hu4_zoneid = unique(hu4.ppt.annual$ZoneID),
+                        hu4_ppt_change = ppt.change)
+
+tmean.change = c()
+for (i in 1:length(unique(hu4.tmean.annual$ZoneID))){
+  dat = hu4.tmean.annual[hu4.tmean.annual$ZoneID == unique(hu4.tmean.annual$ZoneID)[i], ]
+  dat.lm = lm((dat$annual/100)~as.numeric(dat$year))
+  tmean.change[i] = as.numeric(dat.lm$coefficients[2])
+}
+tmean.change = data.frame(hu4_zoneid = unique(hu4.tmean.annual$ZoneID),
+                          hu4_tmean_change = tmean.change)
+
+geo.data = merge(geo.data, ppt.change, by = "hu4_zoneid", all.x = TRUE)
+geo.data = merge(geo.data, tmean.change, by = "hu4_zoneid", all.x = TRUE)
+
+# add ppt and tmean from beginning (1990) and end (2011)
+# of time series. Note timeseries goes until 2013, but 
+# most limno data and all geo data was collected in 2011, so ends there
+
+names(hu4.tmean.annual)[2] = "hu4_zoneid"
+names(hu4.ppt.annual)[2] = "hu4_zoneid"
+
+tmean.1990 = hu4.tmean.annual[hu4.tmean.annual$year == 1990, c("hu4_zoneid", "annual")]
+names(tmean.1990)[2] = "hu4_tmean_1990"
+tmean.2011 = hu4.tmean.annual[hu4.tmean.annual$year == 2011, c("hu4_zoneid", "annual")]
+names(tmean.2011)[2] = "hu4_tmean_2011"
+ppt.1990 = hu4.ppt.annual[hu4.ppt.annual$year == 1990, c("hu4_zoneid", "annual")]
+names(ppt.1990)[2] = "hu4_ppt_1990"
+ppt.2011 = hu4.ppt.annual[hu4.ppt.annual$year == 2011, c("hu4_zoneid", "annual")]
+names(ppt.2011)[2] = "hu4_ppt_2011"
+
+# merge annual temp and precip with other geo data
+geo.data = merge(geo.data, tmean.1990, by = "hu4_zoneid", all.x = TRUE)
+geo.data = merge(geo.data, tmean.2011, by = "hu4_zoneid", all.x = TRUE)
+geo.data = merge(geo.data, ppt.1990, by = "hu4_zoneid", all.x = TRUE)
+geo.data = merge(geo.data, ppt.2011, by = "hu4_zoneid", all.x = TRUE)
+
+#############################
 # import connectivity metrics
 hu4.conn = read.table("hu4_conn.txt", header = TRUE)
 # import land use/land cover data
